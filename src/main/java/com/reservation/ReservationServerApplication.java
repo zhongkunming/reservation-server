a245be437e22202b4ae0908bf82b0587b5a93ed5
zhongkunming <kunmzh@gmail.com>
2023-01-23 18:23:54 +0800

    Initial commit

---
+++ src/main/java/com/reservation/ReservationServerApplication.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/reservation/config/GlobalAppConfig.java
@@ -0,0 +1,20 @@
+package com.reservation.config;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableAsync
+@Configuration
+@EnableScheduling
+@EnableSpringUtil
+@MapperScan(GlobalAppConfig.MAPPER_SCAN_STRING)
+public class GlobalAppConfig {
+    public final static String MAPPER_SCAN_STRING = "com.reservation.mapper";
+
+
+}
